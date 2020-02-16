c1ac128d81020577085919ed490ed16704f017eb
sayannath <sayannath235@gmail.com>
2020-02-16 23:43:24 +0530

    Splash Screen

---
+++ lib/splashScreen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:sky_vsion/homePage.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController _iconAnimationController;
+  CurvedAnimation _iconAnimation;
+
+  void handleTimeOut() {
+    Navigator.of(context).pushReplacement(new MaterialPageRoute(
+        builder: (BuildContext context) => new HomePage()));
+  }
+
+  startTimeout() async {
+    var duration = const Duration(seconds: 5);
+    return new Timer(duration, handleTimeOut);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _iconAnimationController = AnimationController(
+        vsync: this, duration: new Duration(milliseconds: 2000));
+    _iconAnimation = new CurvedAnimation(
+        parent: _iconAnimationController, curve: Curves.easeIn);
+    _iconAnimation.addListener(() => this.setState(() {}));
+    _iconAnimationController.forward();
+    startTimeout();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Container(
+        color: Colors.black,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Smart India Hackathon',
+                style: TextStyle(
+                  fontFamily: "Roboto",
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Sky Vision App',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: "Roboto",
+                ),
+              ),
+              SizedBox(
+                height: 100.0,
+              ),
+              Icon(
+                Icons.camera_alt,
+                color: Colors.green,
+                size: 40.0,
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
